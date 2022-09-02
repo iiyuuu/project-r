5560d301fe4738d385c036ec2b9ff064b47491cd
iiyuuu <nnathan72@yahoo.com>
2022-09-02 03:02:39 -0500

    Add Dash movement and cleaned up player movement

--- Project R/Assets/PlayerControls.cs
+++ Project R/Assets/PlayerControls.cs
@@ -4,21 +4,100 @@ using UnityEngine;
 
 public class PlayerControls : MonoBehaviour
 {
-    //Vector 2 -> 2d Vector with X and Y speed
-    public Vector2 speed = new Vector2(10, 10);
-    public float inputX = Input.GetAxis("Horizontal");
-    public float inputY = Input.GetAxis("Vertical");
-    public current_speed = 2.0f;
+    Rigidbody2D body;
+    public float moveSpeed = 20.0f;
+    private float activeMoveSpeed;
+    public float dashSpeed;
+    public float dashLength = 0.5f;
+    public float dashCooldown = 1f;
+    private Vector2 moveInput;
 
+    private float dashCounter;
+    private float dashCooldownTime;
+
+
+    float moveLimiter = 0.85f;
+    
+
+    //Vector 2 -> 2d Vector with X and Y speed
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        activeMoveSpeed = moveSpeed;
+    }
     //Per Frame basis
     void Update()
-    { 
-        //Vector 3 to contain the total velocity
-        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
-
-        movement *= Time.deltaTime;
-
-        transform.Translate(movement);
+    {
+        PlayerMovement();
+        FixedUpdate();
+        PlayerDash();
     }
+
+    void FixedUpdate()
+    {
+        if(moveInput.x != 0 && moveInput.y != 0)//check if the sprite is moving diagonally
+        {
+            moveInput.x *= moveLimiter;
+            moveInput.y *= moveLimiter;
+        }
+
+        body.velocity = new Vector2(moveInput.x * activeMoveSpeed, moveInput.y * activeMoveSpeed);
+    }
+
+    private void PlayerMovement()
+    {
+
+        bool isIdle = false;
+
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        
+        if(moveInput.x == 0 && moveInput.y == 0)
+        {
+            isIdle = true;
+        }
+
+
+        if (isIdle)
+        {
+            //play animation
+        }
+        else
+        {
+            body.velocity = moveInput * moveSpeed;
+        }
+    }
+
+    private void PlayerDash()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(dashCooldownTime <=0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+
+        }
+
+        if(dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if(dashCounter <= 0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCooldownTime = dashCooldown;
+            }
+        }
+
+        if(dashCooldownTime > 0)
+        {
+            dashCooldownTime -= Time.deltaTime;
+        }
+    }
+
+
 
 }
