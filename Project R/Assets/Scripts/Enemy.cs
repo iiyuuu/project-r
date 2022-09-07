d11ce2a3ace2219c17be12befe22fe9d87fef572
Dylan-Buras <dylan.buras@gmail.com>
2022-09-07 15:48:03 -0500

    Enemy Follows Player

--- Project R/Assets/Scripts/Enemy.cs
+++ Project R/Assets/Scripts/Enemy.cs
@@ -7,13 +7,43 @@ public class Enemy : MonoBehaviour
 
     public int maxHealth = 3;
     private int currentHealth;
+    [SerializeField] float moveSpeed = 25f;
+    private Rigidbody2D rb;
+    public Transform target;
+    private Vector2 moveDirection;
 
+    
 
-    void Start()
+    private void Awake()
     {
-        currentHealth = maxHealth;
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        
+    }
+
+    private void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        moveDirection = direction;
+    }
+    private void FixedUpdate()
+    {
+        moveEnemy(moveDirection);
+    }
+
+    void moveEnemy(Vector2 direction)
+    {
+        rb.MovePosition((Vector2) transform.position + (moveDirection * moveSpeed * Time.deltaTime));
+    }
+
+   
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
