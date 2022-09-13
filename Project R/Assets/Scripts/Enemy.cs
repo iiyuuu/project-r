683c20793431063e521f8e54d4a5f83a5942cd6f
Dylan-Buras <dylan.buras@gmail.com>
2022-09-13 16:59:45 -0500

    Added Player Detection

--- Project R/Assets/Scripts/Enemy.cs
+++ Project R/Assets/Scripts/Enemy.cs
@@ -10,6 +10,8 @@ public class Enemy : MonoBehaviour
     private Rigidbody2D rb;
     public Transform target;
     private Vector2 moveDirection;
+    public float detectionDistance = 10f;
+    private float currentDistance;
 
     public int Health
     {
@@ -37,6 +39,7 @@ public class Enemy : MonoBehaviour
     private void Update()
     {
         Vector3 direction = target.position - transform.position;
+        currentDistance = Vector3.Distance(target.position,transform.position);
         //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         //rb.rotation = angle;
         direction.Normalize();
@@ -44,7 +47,11 @@ public class Enemy : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        moveEnemy(moveDirection);
+        if(currentDistance < detectionDistance)
+        {
+            moveEnemy(moveDirection);
+        }
+        
     }
 
     void moveEnemy(Vector2 direction)
