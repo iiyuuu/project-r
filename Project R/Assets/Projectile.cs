87c25f30563421c7f005020821443c7d00cf1454
iiyuuu <nnathan72@yahoo.com>
2022-09-20 15:32:23 -0500

    Added new scene + NPC

--- Project R/Assets/Projectile.cs
+++ Project R/Assets/Projectile.cs
@@ -4,20 +4,17 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    private Vector3 shootDir;
-    public float moveSpeed = 2f;
+    //private Vector3 shootDir;
+    //public float moveSpeed = 2f;
 
-    public void Setup(Vector3 shootDir)
-    {
-<<<<<<< Updated upstream
-        this.shootDir = shootDir;
-=======
-        rb.MovePosition(transform.right * speed * Time.deltaTime);
->>>>>>> Stashed changes
-    }
+    //public void Setup(Vector3 shootDir)
+    //{
+    //    this.shootDir = shootDir;
+    //    rb.MovePosition(transform.right * speed * Time.deltaTime);
+    //}
 
-    private void Update()
-    {
-        transform.position += shootDir * moveSpeed * Time.deltaTime;
-    }
+    //private void Update()
+    //{
+    //    transform.position += shootDir * moveSpeed * Time.deltaTime;
+    //}
 }
