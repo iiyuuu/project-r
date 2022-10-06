406ff673da2f83d8a979f1e99fb40ccd36039d17
tqvu <tqvu29@gmail.com>
2022-10-05 23:27:10 -0500

    Add loading scene on F1

--- Project R/Assets/Scripts/SceneTeleporter.cs
+++ Project R/Assets/Scripts/SceneTeleporter.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Linq;
+using UnityEngine.UI;
 
 public class SceneTeleporter : MonoBehaviour
 {
 
     public string sceneToLoad;
+    public GameObject loadingScreen;
+    public Slider loadingBar;
     public Vector2 playerPosition;
     List<string> usedScenes = new List<string>(); //keeps track of used scenes
     bool isDuplicate = true;
@@ -51,9 +54,22 @@ public class SceneTeleporter : MonoBehaviour
                     isDuplicate = false;
                 }
             }
-            
+
             //playerStorage.initialValue = playerPosition;
-            SceneManager.LoadScene(sceneToLoad);
+            //SceneManager.LoadScene(sceneToLoad);
+
+            StartCoroutine(LoadSceneAsynchronously(sceneToLoad));
+        }
+    }
+
+    IEnumerator LoadSceneAsynchronously(string sceneToLoad)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneToLoad);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            loadingBar.value = operation.progress;
+            yield return null;
         }
     }
 }
