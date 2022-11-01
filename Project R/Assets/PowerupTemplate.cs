258435e65381ea43902bdf321f1b3dec901943bc
iiyuuu <nnathan72@yahoo.com>
2022-10-31 23:02:59 -0500

    final build for tuesday meeting

---
+++ Project R/Assets/PowerupTemplate.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PowerupTemplate : MonoBehaviour
+{
+    public TMP_Text titleText;
+    public TMP_Text descriptionText;
+}
